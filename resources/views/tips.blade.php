7227f2a7d4bd7072c21436b7465835ee6a739fa4
Jonathan Duran <headjd@gmail.com>
2018-06-30 18:12:14 -0400

    Section Tips

--- resources/views/tips.blade.php
+++ resources/views/tips.blade.php
@@ -6,7 +6,82 @@
 @include('include.navbar')
 
 <!-- Section Tips -->
+<section id="tips">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Tips</h1>
+        <hr>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card-columns">
+          <div class="card">
+            <img class="card-img-top" src="{{ asset('img/last-garage.png') }}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Tips title</h5>
+              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img class="card-img-top" src="{{ asset('img/last-garage.png') }}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Tips title</h5>
+              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img class="card-img-top" src="{{ asset('img/last-garage.png') }}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Tips title</h5>
+              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img class="card-img-top" src="{{ asset('img/last-garage.png') }}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Tips title</h5>
+              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img class="card-img-top" src="{{ asset('img/last-garage.png') }}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Tips title</h5>
+              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
 
-@include('include.footer')
+      </div>
+    </div>
+    <div class="col-md-12">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</section>
 
-@endsection
+@include('include.footer') @endsection
