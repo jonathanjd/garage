90e48d949810b77af49940690f15c7a907b37c0f
Jonathan Duran <headjd@gmail.com>
2018-06-30 15:37:34 -0400

    Add Bootstrap 4

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.base')
+@section('title', 'Home')
+
+@section('content')
+    <h1>App Section</h1>
+@endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+
+    <title>App Garage @yield('title')</title>
+  </head>
+  <body>
+    @yield('content')
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="asset('js/jquery-3.3.1.min.js')"></script>
+    <script src="asset('js/popper.min.js')"></script>
+    <script src="asset('js/bootstrap.min.js')"></script>
+  </body>
+</html>
